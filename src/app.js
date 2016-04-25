8ed8cdeefb8494f92a4e7824e035c800d80fdfb2
Matthew Pateman <matthewpateman@me.com>
2016-04-25 16:17:48 +0100

    formatted message

--- src/app.js
+++ src/app.js
@@ -58,7 +58,27 @@ function processEvent(event) {
             // }
 
              console.log('Response as text message');
-                    sendFBMessage(sender, {attachment: { type: 'image', payload: { url: 'http://static1.squarespace.com/static/56732772df40f3fd5f559f5d/t/56c399a87da24f0e52c17a65/1455659432519/aesthetics.jpg?format=500w' }}});
+                    sendFBMessage(sender, {
+                        attachment: { 
+                            type: 'template', 
+                                payload:{
+                                    template_type: 'button',
+                                    text: 'What do you want to do next?',
+        buttons:[
+          {
+            type:"web_url",
+            url:"https://petersapparel.parseapp.com",
+            title:"Show Website"
+          },
+          {
+            type:"postback",
+            title:"Start Chatting",
+            payload:"USER_DEFINED_PAYLOAD"
+          }
+        ]
+      }
+                        }
+                    );
 
 
 
