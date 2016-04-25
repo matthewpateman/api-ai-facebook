670c56974a7e2a2778889c2c0756dd6b3997fe06
Matthew Pateman <matthewpateman@me.com>
2016-04-25 14:13:54 +0100

    Updated Logs

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ function processEvent(event) {
                         sendFBMessage(sender, {text: err.message });
                     }
                 } else if (isDefined(responseText)) {
-                    console.log('Response as text message');
+                    console.log('Response as text message:' + responseText);
                     sendFBMessage(sender, {text: responseText});
                 }
 
